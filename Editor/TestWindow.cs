8b452ca668dc73223b312bc2e9ad7aa017314161
lcool <leechengdragon@qq.com>
2019-05-02 10:46:05 +0800

    init

---
+++ Editor/TestWindow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class TestWindow : EditorWindow
+{
+    [MenuItem("Test/TestWindow")]
+    public static void ShowWindow()
+    {
+        GetWindow<TestWindow>().Show();
+    }
+}
